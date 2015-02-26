4e679e21ee32f03a4b343d1a1536766f36522acb
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2015-02-26 14:19:43 +0000

    Tornar a http

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ app.use("/api/sessions", require("./controllers/api/sessions"));
 app.use("/api/users", require("./controllers/api/users"));
 app.use(require("./auth"));
 app.use("/",require("./controllers/static"));
-/*
+
 app.listen(process.env.PORT, function() {
     console.log('Server listening on ', process.env.PORT);
 });
-*/
-app.listen =  function() {
+
+/*app.listen =  function() {
     var server = https.createServer(this).listen;
     return server.listen(process.env.PORT,process.env.IP).apply(server, function () {
         console.log('Server listening on', process.env.PORT);
         
     });
     
-};
+};*/
 //var patata = https.createServer(app);
 //patata.listen(process.env.PORT, process.env.IP);
 //var server = https.createServer(https_options, app).listen(process.env.PORT, process.env.IP);
