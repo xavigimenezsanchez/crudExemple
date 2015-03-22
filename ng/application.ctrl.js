ef04ba585c869891c0dfba0148152e8e89799e74
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2015-03-22 13:22:43 +0000

    add comments to application.ctrl.js

--- ng/application.ctrl.js
+++ ng/application.ctrl.js
@@ -1,9 +1,20 @@
 angular.module('appLearn')
     .controller("ApplicationController", function($scope,$location,UserSvc) {
         $scope.$on('login', function(e,user) {
+            /*
+                Quan s'ha fet login s'emet l'event "login"
+                i això fa que la variable de l'scope "currentUser"
+                li diem quin usuari s'ha autenticant, d'aquesta manera
+                fem que apareguin diferents opcions al menú
+            */
             $scope.currentUser = user;
         });
         $scope.logout = function(){
+            /*
+                Quan fem logout esborrem el token i la variable
+                de l'$scope "currentUser", d'aquesta forma desapareixen
+                els menús sensibles a la autenticació
+            */
             UserSvc.logOut();
             delete $scope.currentUser;
             $location.path('/');
