887f6c7cec66e6cbb60503841345e7d27c1f7b3b
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2015-03-22 12:26:24 +0000

    add comments to user.srv.js

--- ng/user.srv.js
+++ ng/user.srv.js
@@ -1,29 +1,46 @@
 angular.module('appLearn')
     .service('UserSvc', function($http) {
-        this.auth= false;
-        this.getUser = function() {
+        var srv = this;
+        srv.auth= false;
+        srv.getUser = function() {
             return $http.get('/api/users');
         };
-        this.login = function (username, password,noLogin) {
+        srv.login = function (username, password,noLogin) {
             return $http.post('/api/sessions', {
                 username: username,
                 password: password
             }).success(function(data,status) {
-                svc.token = data;
+                /*
+                    Si l'autenticació és correcte li diem a l'angular que cada 
+                    vegada que es comuniqui amb el servidor afegeixi el token 
+                    al header 'x-auth'
+                */
                 $http.defaults.headers.common['x-auth'] = data;
-                if (data) svc.auth = true;
+                if (data) srv.auth = true;
             }).error(function(error,status){
-                noLogin(error, status);  //s'executa el callback
+                /*
+                    Si l'usuari i contrasenya no és correcte executa la
+                    función callback que li hem passat com paràmetre
+                */
+                noLogin(error, status);
             });
         };
         this.registre = function(username,password){
+            /*
+                Per registrar un usuari nou, només hem de fer un post
+                a l'api d'usuaris
+            */
             return $http.post('/api/users', {
                 username: username,
                 password: password
             });
         };
         this.logOut = function() {
-            svc.auth = false;
+            /*
+                Quan l'usuari fa logout s'esborra el token
+                i posem la propietat del servei "auth" a false
+            */
+            srv.auth = false;
             $http.defaults.headers.common['x-auth'] ="";
         };
     });
